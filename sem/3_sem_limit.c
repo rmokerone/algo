97d81b7a9b5285ced7658f4e2100735be367c330
vroot <root@oopy.org>
2021-05-23 18:53:01 +0800

    打印信号量限制值

--- sem/3_sem_limit.c
+++ sem/3_sem_limit.c
@@ -5,6 +5,8 @@ int main() {
     long sem_nsems_max = sysconf(_SC_SEM_NSEMS_MAX);
     long sem_value_max = sysconf(_SC_SEM_VALUE_MAX);
 
-    printf("sem_nsems_max: %ld\n", sem_nsems_max);
+    printf("`: %ld\n", sem_nsems_max);
     printf("sem_value_max: %ld\n", sem_value_max);
 }
+
+
