e459e6a866a7209f9a30cb6bdb848fd2fb9d003b
江柳 <root@oopy.org>
2021-07-14 16:39:15 +0800

    增加plog示例

---
+++ 09_plog/test_plog.cpp
@@ -0,0 +1,20 @@
+#include <plog/Log.h> // Step1: include the headers
+
+int main()
+{
+    plog::init(plog::debug, "Hello.txt"); // Step2: initialize the logger
+
+    // Step3: write log messages using a special macro
+    // There are several log macros, use the macro you liked the most
+
+    PLOGD << "Hello log!"; // short macro
+    PLOG_DEBUG << "Hello log!"; // long macro
+    PLOG(plog::debug) << "Hello log!"; // function-style macro
+    
+    // Also you can use LOG_XXX macro but it may clash with other logging libraries
+    LOGD << "Hello log!"; // short macro
+    LOG_DEBUG << "Hello log!"; // long macro
+    LOG(plog::debug) << "Hello log!"; // function-style macro
+
+    return 0;
+}
